3e60aa1fcff4e1826488f2b650d56a226528188a
hieugch18038 <hieunhgch18038@fpt.edu.vn>
2022-08-19 13:57:58 +0700

    Connect Bookcontroller to database

---
+++ AsmApp/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using AsmApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AsmApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext context;
+        public BookController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var books = context.Book.ToList();
+            return View(books);
+        }
+        public IActionResult Store()
+        {
+            return View(context.Book.ToList());
+        }
+    }
+}
