b21f4b3366ef8b2e789c929dd86744edf3830118
hieugch18038 <hieunhgch18038@fpt.edu.vn>
2022-08-19 14:25:11 +0700

    Checking view book detail

--- AsmApp/Controllers/AuthorController.cs
+++ AsmApp/Controllers/AuthorController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AsmApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AsmApp.Controllers
 {
     public class AuthorController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext context;
+        public AuthorController(ApplicationDbContext applicationDbContext)
         {
-            return View();
+            context = applicationDbContext;
+        }
+        public IActionResult Detail()
+        {
+            var authors = context.Author.ToList();
+            return View(authors);
         }
     }
 }
